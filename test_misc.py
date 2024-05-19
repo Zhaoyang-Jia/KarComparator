9f2e2cede7eb9ec01017f2894bb55673bdb77033
zhaoyangjia <z4jia@ucsd.edu>
2024-05-18 17:02:07 -0700

    test on new visualization

--- test_misc.py
+++ test_misc.py
@@ -1,65 +1,53 @@
-from scipy.optimize import linear_sum_assignment
-import numpy as np
-import networkx as nx
-import netgraph as ng
 import matplotlib.pyplot as plt
-from utils import *
-import os
+import matplotlib.patches as patches
 
-# uniform_dist = 1 / 6
-# graph_width = uniform_dist * 100
-# width_multiplier = 2.5
-# height_multiplier = 1
-#
-# G = nx.DiGraph()
-# G.add_nodes_from(['0$', '0#'])
-# G.add_edge('0$', '0#', weight=2)
-#
-# V_pos = {'0$': (0.0, 0.5), '0#': (1.0, 0.5)}
-# E_weights = {('0$', '0#'): 2}
-# E_pos = {('0$', '0#'): generate_parabola(0.0, 1.0, 0.5, 2)}
-#
-# plt.figure(figsize=(graph_width * width_multiplier, graph_width * height_multiplier))
-# plot_karsim = ng.InteractiveGraph(G,
-#                                   node_layout=V_pos,
-#                                   node_labels=True,
-#                                   edge_layout=E_pos,
-#                                   edge_labels=E_weights,
-#                                   arrows=True,
-#                                   node_size=6,
-#                                   node_label_offset=0.001,
-#                                   node_label_font_dict=dict(size=10),
-#                                   edge_label_fontdict=dict(size=9),
-#                                   scale=(graph_width, 1))
-# plt.savefig('test_merged_plot.png')
-
-# x = ''
-# print(len(x))
-
-import re
+# Example data for one chromosome
+chromosome_data = {
+    'name': 'chr1',
+    'bands': [
+        {'start': 0, 'end': 15, 'name': 'p15', 'color': 'lightgrey'},
+        {'start': 15, 'end': 25, 'name': 'p14', 'color': 'grey'},
+        {'start': 25, 'end': 35, 'name': 'p13', 'color': 'lightgrey'},
+        {'start': 35, 'end': 45, 'name': 'p12', 'color': 'grey'},
+        {'start': 45, 'end': 55, 'name': 'p11.2', 'color': 'red'},
+        {'start': 55, 'end': 65, 'name': 'q11.2', 'color': 'blue'},
+        {'start': 65, 'end': 75, 'name': 'q21', 'color': 'black'},
+        {'start': 75, 'end': 85, 'name': 'q22', 'color': 'white'},
+        {'start': 85, 'end': 95, 'name': 'q23', 'color': 'grey'},
+        {'start': 95, 'end': 105, 'name': 'q24', 'color': 'white'},
+        {'start': 105, 'end': 115, 'name': 'q25', 'color': 'grey'},
+        {'start': 115, 'end': 125, 'name': 'q26', 'color': 'white'}
+    ]
+}
 
 
-input_string0 = "Chr1: 237,668,707-248,943,333"
-input_string1 = "Balanced translocation between Chr1 and Chr17, between segments Chr1: 237,668,707-248,943,333 (q43 - q44) and Chr17: 80,544,491-83,246,392 (q25.3 - q25.3)"
-input_string2 = "Deletion on Chr2: 391-97,531,656 (q11.2 - q11.2)"
+def plot_chromosome(ax, chromosome_data, y_offset=0):
+    for band in chromosome_data['bands']:
+        start = band['start']
+        end = band['end']
+        name = band['name']
+        color = band['color']
 
-pattern = r'Chr(\d+): (\d{1,3}(?:,\d{3})*)-(\d{1,3}(?:,\d{3})*) \(.*?\)'
+        rect = patches.Rectangle((start, y_offset), end - start, 1, linewidth=1, edgecolor='black', facecolor=color)
+        ax.add_patch(rect)
+        ax.text((start + end) / 2, y_offset + 0.5, name, ha='center', va='center', fontsize=8, color='blue')
 
-# Extract integers using regex
-matches = re.finditer(pattern, input_string1)
-
-for match_itr in matches:
-    print(match_itr.group(1))
-    print(match_itr.start())
-    print(match_itr.end())
-    print(input_string1[match_itr.start(): match_itr.end()])
+    ax.text(130, y_offset + 0.5, chromosome_data['name'], va='center', fontsize=12)
+    ax.set_xlim(0, 140)
+    ax.set_ylim(-1, 2)
+    ax.axis('off')
 
 
-# if __name__ == "__main__":
-#     path = 'latex_reports/paul_dremsek_plots/002.pdf'
-#     # path = '/media/zhaoyang-new/workspace/KarSim/KarComparator/latex_reports/sunnyside_plots/2.pdf'
-#     if os.path.exists(path):
-#         print('x')
-#     else:
-#         print('y')
+fig, ax = plt.subplots(figsize=(10, 2))
 
+plot_chromosome(ax, chromosome_data)
+
+# Rotate the entire plot 90 degrees counterclockwise
+plt.gca().set_aspect('equal', adjustable='box')
+plt.gca().invert_yaxis()
+plt.gca().invert_xaxis()
+plt.gca().set_xlim(ax.get_xlim()[::-1])
+plt.gca().set_ylim(ax.get_ylim()[::-1])
+plt.gca().transData.rotate_deg(90)
+
+plt.show()
